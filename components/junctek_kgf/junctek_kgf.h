60e55b6427b19003c7114a336f5d20afa845cbf0
photoreal3d <44806230+photoreal3d@users.noreply.github.com>
2026-04-02 11:59:42 +0500

    Update junctek_kgf.h

--- components/junctek_kgf/junctek_kgf.h
+++ components/junctek_kgf/junctek_kgf.h
@@ -19,29 +19,30 @@ public:
   void set_update_settings_interval(uint32_t interval) { update_settings_interval_ = interval; }
   void set_update_stats_interval(uint32_t interval) { update_stats_interval_ = interval; }
   void set_current_direction_sensor(sensor::Sensor *current_direction_sensor) { current_direction_sensor_ = current_direction_sensor; }
-  void set_battery_ohm_sensor(sensor::Sensor *battery_ohm_sensor) { battery_ohm_sensor_ = battery_ohm_sensor; }
   void set_battery_level_sensor(sensor::Sensor *battery_level_sensor) { battery_level_sensor_ = battery_level_sensor; }
   void set_amp_hour_remain_sensor(sensor::Sensor *amp_hour_remain_sensor) { amp_hour_remain_sensor_ = amp_hour_remain_sensor; }
-  void set_amp_hour_used_total_sensor(sensor::Sensor *amp_hour_used_sensor) { amp_hour_used_sensor_ = amp_hour_used_sensor; }
-  void set_amp_hour_charged_total_sensor(sensor::Sensor *amp_hour_charged_sensor) { amp_hour_charged_sensor_ = amp_hour_charged_sensor; }
-  void set_battery_charged_energy_sensor(sensor::Sensor *battery_charged_energy_sensor) { battery_charged_energy_sensor_ = battery_charged_energy_sensor; }
-  void set_battery_discharged_energy_sensor(sensor::Sensor *battery_discharged_energy_sensor) { battery_discharged_energy_sensor_ = battery_discharged_energy_sensor; }
+  
+  // Энергия (кВт*ч)
+  void set_energy_total_discharged_sensor(sensor::Sensor *energy_sensor) { energy_total_discharged_sensor_ = energy_sensor; }
+  void set_energy_total_charged_sensor(sensor::Sensor *energy_sensor) { energy_total_charged_sensor_ = energy_sensor; }
+  
+  // Мощность (Вт)
+  void set_battery_charged_power_sensor(sensor::Sensor *power_sensor) { battery_charged_power_sensor_ = power_sensor; }
+  void set_battery_discharged_power_sensor(sensor::Sensor *power_sensor) { battery_discharged_power_sensor_ = power_sensor; }
+  
   void set_output_status_sensor(sensor::Sensor *output_status_sensor) { output_status_sensor_ = output_status_sensor; }
   void set_power_sensor(sensor::Sensor *power_sensor) { power_sensor_ = power_sensor; }
   void set_battery_life_sensor(sensor::Sensor *battery_life_sensor) { battery_life_sensor_ = battery_life_sensor; }
-  void set_runtime_sensor(sensor::Sensor *runtime_sensor) { runtime_sensor_ = runtime_sensor; }
 
   void dump_config() override;
   void loop() override;
-  float get_setup_priority() const;// override;
+  float get_setup_priority() const;
 
 protected:
   bool readline();
   void handle_line();
   void handle_status(const char* buffer);
   void handle_settings(const char* buffer);
-  void request_data(uint8_t data_id);
-  void decode_data(std::vector<uint8_t> data);
   bool verify_checksum(int checksum, const char* buffer);
 
   const unsigned address_;
@@ -52,22 +53,20 @@ protected:
   sensor::Sensor* current_sensor_{nullptr};
   sensor::Sensor* temperature_{nullptr};
   sensor::Sensor* current_direction_sensor_{nullptr};
-  sensor::Sensor* battery_ohm_sensor_{nullptr};
   sensor::Sensor* battery_level_sensor_{nullptr};
-
   sensor::Sensor* amp_hour_remain_sensor_{nullptr};
-  sensor::Sensor* amp_hour_used_sensor_{nullptr};
-  sensor::Sensor* amp_hour_charged_sensor_{nullptr};
-  sensor::Sensor* battery_charged_energy_sensor_{nullptr};
-  sensor::Sensor* battery_discharged_energy_sensor_{nullptr};
+
+  sensor::Sensor* energy_total_discharged_sensor_{nullptr};
+  sensor::Sensor* energy_total_charged_sensor_{nullptr};
+  sensor::Sensor* battery_charged_power_sensor_{nullptr};
+  sensor::Sensor* battery_discharged_power_sensor_{nullptr};
 
   sensor::Sensor* output_status_sensor_{nullptr};
   sensor::Sensor* power_sensor_{nullptr};
-  sensor::Sensor* runtime_sensor_{nullptr};
   sensor::Sensor* battery_life_sensor_{nullptr};
 
   static constexpr int MAX_LINE_LEN = 120;
-  std::array<char, MAX_LINE_LEN> line_buffer_;
+  char line_buffer_[MAX_LINE_LEN];
   size_t line_pos_ = 0;
 
   optional<float> battery_capacity_;
